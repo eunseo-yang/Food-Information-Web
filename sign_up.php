1e63d415b5e949bc24a2342220fa93cb507aec20
eunseo1092 <55356269+eunseo1092@users.noreply.github.com>
2020-11-19 03:28:08 +0900

    Add files via upload

--- sign_up.php
+++ sign_up.php
@@ -7,6 +7,8 @@
    $height = $_POST['height'];
    $weight = $_POST['weight'];
    $gender = $_POST['gender'];
+   $information = $_POST['information'];
+   $item = $_POST['item'];
 
    if($id == NULL || $pwd == NULL || $name == NULL || $height == NULL || $weight == NULL || $gender == NULL){
       echo "정보가 빠져있습니다. 모든 정보를 채워주세요";
@@ -37,4 +39,56 @@
       echo "에러가 발생하였습니다"."<br>";
       echo $mysqli->error;
    }
+
+   $check2 = "UPDATE nutrition set [$item]=$information WHERE name = '$name'";
+	$result2 = $mysqli->query($check2);
+
+    if($result2!=null){
+        $row = $result2->fetch_array(3);
+        $a=$row['menu'];
+        echo $a;
+
+       
+    }
+    else echo "입력하신 음식을 찾을 수 없습니다";
+
+    $id = "SELECT id from nutrition where name='$name'"; 
+    $check3 = "DELETE FROM nutrition  WHERE num='$id'";
+	$result3 = $mysqli->query($check3);
+
+
 ?>
+
+
+<form method='post' action="food_info_result.php">
+            <section class="login-input-section-wrap">
+                <h2>You can update your information. Try it!</h2> 
+            </section>
+            <section class="login-input-section-wrap">
+                <div class="login-input-wrap">	
+                    <input placeholder="Input item you want to update." type="text" id="item" name="item"></input>
+                </div>
+                <div class="login-input-wrap">	
+                    <input placeholder="Input information you want to update." type="text" id="information" name="information"></input>
+                </div>
+            </section>
+            <section class="Easy-sgin-in-wrap">
+                <ul class="login-button-wrap">
+                    </li><button type='submit'>Update</button>
+                </ul>
+            </section>
+        </form>
+        
+        <br>
+        <br>
+        <br>
+        <form method='post' action="food_info_result.php">
+            <section class="login-input-section-wrap">
+                <h2>You can delete your account.</h2> 
+            </section>
+            <section class="Easy-sgin-in-wrap">
+                <ul class="login-button-wrap">
+                    </li><button type='submit' id="delete" name="delete">Delete</button>
+                </ul>
+            </section>
+        </form>
