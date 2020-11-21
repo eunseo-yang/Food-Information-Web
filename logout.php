6d21572113b862a573a40e09d6439561710ff62d
jyoon186 <74604994+jyoon186@users.noreply.github.com>
2020-11-22 01:50:55 +0900

    Add files via upload

--- logout.php
+++ logout.php
@@ -3,8 +3,8 @@
 
 	session_start();
 
-	if(isset($_SESSION['userid'] )){
-		unset($_SESSION['userid']);
+	if(isset($_SESSION['id'] )){
+		unset($_SESSION['id']);
 		session_destroy();
 		echo"<script>alert('로그아웃 되었습니다');location.href= 'main_page.html';</script>";
 
